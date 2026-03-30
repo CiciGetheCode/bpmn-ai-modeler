0ba097918e27b29fa7ce5a5a7263c770eca682e6
CicigettheCode <aggelostsitsi@gmail.com>
2026-03-30 15:57:24 +0300

    correctvariables

---
+++ public/env.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  FLOW_ID: "fba145af-342e-4bca-ba69-cda695283968",
+  HOST_URL:"https://bpmnaiagenthelper.politemoss-c1da2b8d.westeurope.azurecontainerapps.io",
+  LANGFLOW_API_KEY: "sk-rWrUB-6rrzP3M-Uae9WjwjB7CQMf9yP6NNuQuwb9XHs"
+};
